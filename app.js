008b5814a5bcdc7588ca1ee2785dac475e86da04
randeer <randeerlalanga92@gmail.com>
2020-01-18 15:06:23 +0530

    addtribute-bind

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Dilshani Lakshika Dias'
+        title: 'Dilshani Lakshika Dias',
+        linkdin: 'https://lk.linkedin.com/in/dilshani-dias-91257a166'
     },
     methods: {
         testing(){
