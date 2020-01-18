e067606e73c83e56ad861f620977dc39443eaf9e
randeer <randeerlalanga92@gmail.com>
2020-01-18 14:45:05 +0530

    one-done-1

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ new Vue({
     el: '#app',
     data: {
         title: 'Dilshani Lakshika Dias'
+    },
+    methods: {
+        testing(){
+            return 'I am currently working on Dialog';
+        }
     }
 })
